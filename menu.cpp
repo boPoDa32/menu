0d08f22d9a0cbf34cc8ac6e71b8e5491db7c71aa
Yuriy <smouke94@bigmir.net>
2013-05-20 15:49:48 +0300

    20.05

--- menu.cpp
+++ menu.cpp
@@ -1,10 +1,31 @@
-class Menu
-{
-private:
-  string nameprod;
-	int weight;
-	int number;
-public:
+#include "stdafx.h"
+#include <string>
+#include "Menu.h"
+#include <iostream>
+using namespace std;
 
-void set (string,int,int)
-};
+Menu::Menu(){}
+
+Menu::Menu(string nameprod,int weight,int number)
+{
+nameprod = "0";
+weight = 1;
+number = 2;
+}
+
+void Menu::set(string nameprdcs, int wess,int chislo)
+{
+nameprod = nameprdcs;
+weight = wess;
+number = chislo;
+}
+
+int Menu::get()
+{
+return number;
+}
+
+void Menu::displayMenu()
+{
+	cout<<get();
+}
